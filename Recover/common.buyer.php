84e256d748226d56f9e675d1d56efad41319099e
Hsu Yee Mon <hsuyeemon.1@gmail.com>
2018-08-13 00:31:35 +0630

    language function

--- Recover/common.buyer.php
+++ Recover/common.buyer.php
@@ -40,11 +40,11 @@ function displayNavBuyer(){
   
   <!-- Dropdown Structure -->
 
-  <!--Language------------------>
+    <!--Language------------------>
   <ul id="font" class="dropdown-content">
-    <li><a href="#" id="myanmar" onclick="language()">ျမန္မာစာ</a></li>
+    <li><a href="index.php?lan_flag=1"  id="myanmar">ျမန္မာစာ</a></li>
     <li class="divider"></li>
-    <li><a href="#" id="english" onclick="">English</a></li>
+    <li><a href="index.php?lan_flag=0" id="english">English</a></li>
   </ul>
 
   <!---Navigation-->
@@ -55,7 +55,7 @@ function displayNavBuyer(){
 
 
       <li><a class="dropdown-trigger" href="#!" data-target="font" id="language">Language<i class="material-icons right">arrow_drop_down</i></a></li>
-      <li><a href="index.php" id="home">Home</a></li>
+      <li><a href="index.php" id="home1">Home</a></li>
       
       <li><a href="index.php#aboutus" id="about_as">About Us</a></li>
       <li><a href="index.php#contactus" id="contact">Contact</a></li-->
@@ -93,5 +93,46 @@ function displayNavBuyer(){
   </div>
 </nav>
 <?php
+}
+
+
+  //session_start(); 
+if(isset($_GET['lan_flag'])){
+$_SESSION['lan_flag']=$_GET['lan_flag'];
+}
+$lan_flag=null;
+echo $_SESSION['lan_flag'];
+if(isset($_SESSION['lan_flag'])){
+$lan_flag=$_SESSION['lan_flag'];
+}
+
+  if ($lan_flag) {
+ ?>
+<script type="text/javascript">
+  language();
+  function language(){
+   
+    document.getElementById('home1').innerHTML="ပင္မ စာမ်က္ႏွာ";
+     document.getElementById('language').innerHTML="ဘာသာစကား";  
+    document.getElementById("products").innerHTML="ကုန္ပစၥည္း မ်ား ";
+    document.getElementById("about_as").innerHTML="ကြၽႏုပ္တို႔ အေၾကာင္း";
+    document.getElementById("contact").innerHTML="ဆက္သြယ္ရန္";
+    document.getElementById("login1").innerHTML="အေကာင့္ ဝင္ရန္ ";
+    document.getElementById("login_dropdown").innerHTML="အေကာင့္ ဝင္ရန္ ";
+    document.getElementById("product_dropdown").innerHTML="ကုန္ပစၥည္း မ်ား";
+    document.getElementById("my_product").innerHTML="မွာယူထားေသာပစၥည္းမ်ား";
+    document.getElementById("my_order").innerHTML="မွာယူရန္စာရင္း";
+    document.getElementById("cart").innerHTML="ေၾကာ္ျငာထားေသာပစၥည္းမ်ား";
+
+    
+    document.getElementById("user_name").innerHTML="အမည္";
+    document.getElementById("switch_account").innerHTML="အေကာင့္ခ်ိန္းရန္";
+    document.getElementById("logout").innerHTML="အေကာင့္ထြက္ရန္";
+    document.getElementById("contact_us").innerHTML="ဆက္သြယ္ရန္";
+    document.getElementById("about_as1").innerHTML="ကြၽႏုပ္တို႔ အေၾကာင္း";
+
+  }  
+</script> 
+  <?php
 }
 ?>
--- Recover/common.seller.php
+++ Recover/common.seller.php
@@ -84,11 +84,11 @@ function displayNavSeller(){
   
   <!-- Dropdown Structure -->
 
-  <!--Language------------------>
+    <!--Language------------------>
   <ul id="font" class="dropdown-content">
-    <li><a href="#" id="myanmar" onclick="language()">ျမန္မာစာ</a></li>
+    <li><a href="index.php?lan_flag=1"  id="myanmar">ျမန္မာစာ</a></li>
     <li class="divider"></li>
-    <li><a href="#" id="english" onclick="">English</a></li>
+    <li><a href="index.php?lan_flag=0" id="english">English</a></li>
   </ul>
 
  
@@ -137,6 +137,46 @@ function displayNavSeller(){
 <!-- end of mobile nav  -->
   </div>
 </nav>
-
 <?php
+
+  //session_start(); 
+if(isset($_GET['lan_flag'])){
+$_SESSION['lan_flag']=$_GET['lan_flag'];
 }
+$lan_flag=null;
+echo $_SESSION['lan_flag'];
+if(isset($_SESSION['lan_flag'])){
+$lan_flag=$_SESSION['lan_flag'];
+}
+
+  if ($lan_flag) {
+ ?>
+<script type="text/javascript">
+  language();
+  function language(){
+   
+    document.getElementById('home1').innerHTML="ပင္မ စာမ်က္ႏွာ";
+     document.getElementById('language').innerHTML="ဘာသာစကား";  
+    document.getElementById("products").innerHTML="ကုန္ပစၥည္း မ်ား ";
+    document.getElementById("about_as").innerHTML="ကြၽႏုပ္တို႔ အေၾကာင္း";
+    document.getElementById("contact").innerHTML="ဆက္သြယ္ရန္";
+    document.getElementById("login1").innerHTML="အေကာင့္ ဝင္ရန္ ";
+    document.getElementById("login_dropdown").innerHTML="အေကာင့္ ဝင္ရန္ ";
+    document.getElementById("product_dropdown").innerHTML="ကုန္ပစၥည္း မ်ား";
+    document.getElementById("my_product").innerHTML="မွာယူထားေသာပစၥည္းမ်ား";
+    document.getElementById("my_order").innerHTML="မွာယူရန္စာရင္း";
+    document.getElementById("cart").innerHTML="ေၾကာ္ျငာထားေသာပစၥည္းမ်ား";
+
+    
+    document.getElementById("user_name").innerHTML="အမည္";
+    document.getElementById("switch_account").innerHTML="အေကာင့္ခ်ိန္းရန္";
+    document.getElementById("logout").innerHTML="အေကာင့္ထြက္ရန္";
+    document.getElementById("contact_us").innerHTML="ဆက္သြယ္ရန္";
+    document.getElementById("about_as1").innerHTML="ကြၽႏုပ္တို႔ အေၾကာင္း";
+
+  }  
+</script> 
+  <?php
+}
+}
+?>
