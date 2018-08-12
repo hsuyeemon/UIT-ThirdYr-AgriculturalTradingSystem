558119c61cb8caead88499bea7457b9e1e0bb5ed
Nyein Chan <einghonphoo@uit.edu.mm>
2018-08-12 13:31:29 +0630

    aa

--- my_php_functions/db_config.php
+++ my_php_functions/db_config.php
@@ -1,10 +1,10 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '','agriculture_system'); 
+$connection = mysqli_connect('localhost', 'root', '','agri_project'); 
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
 
-$select_db = mysqli_select_db($connection, 'agriculture_system');
+$select_db = mysqli_select_db($connection, 'agri_project');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection)); 
 }
